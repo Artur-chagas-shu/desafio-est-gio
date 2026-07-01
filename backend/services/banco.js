429b8de6611a999e503af113cf289a82c4f5f7ab
Artur Chagas <arturchagax@gmail.com>
2026-06-30 22:06:09 -0300

    Criando a funcao criarConta

---
+++ backend/services/banco.js
@@ -0,0 +1,21 @@
+const {v4: uuidv4} = require('uuid');
+
+const contas = [];
+
+function criarConta(tipo, saldoInicial = 0){
+    if(!['corrente', 'poupanca'].includes(tipo)){
+        throw new Error('Tipo de conta inválido. Deve ser "corrente" ou "poupanca".');
+    }
+    if(saldoInicial < 0){
+        throw new Error('Saldo inicial não pode ser negativo.');
+    }
+
+    const novaConta = {
+        id: uuidv4(),
+        tipo,
+        saldo: saldoInicial
+    };
+    contas.push(novaConta);
+    return novaConta;
+}
+
