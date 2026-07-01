57bf178d9a3a929b1d88b3c5e2715201f8aca49f
Artur Chagas <arturchagax@gmail.com>
2026-06-30 22:20:26 -0300

    Corrigindo erro

--- backend/routes/contas.js
+++ backend/routes/contas.js
@@ -5,7 +5,7 @@ const banco = require('../services/banco');
 router.post('/', (req, res) => {
     try {
         const { tipo, saldoInicial } = req.body;
-        const conta = banco,criarConta(tipo, saldoInicial);
+        const conta = banco.criarConta(tipo, saldoInicial);
         res.status(201).json(conta);
     }catch(err){
         res.status(400).json({ error: err.message });
